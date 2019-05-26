0bb7d72456c93e7ef3106653488562a9fe787bee
Lai <laialexanderholmsen@gmail.com>
2019-05-26 18:01:05 +0200

    Add first draft of quiz

--- quiz.py
+++ quiz.py
@@ -4,5 +4,45 @@ import random
 with open("countries_info.json", "r") as file:
     COUNTRIES = json.load(file)
 
-COUNTRY = random.choice(COUNTRIES)
-print(f"{COUNTRY['capital']} is the capital of {COUNTRY['name']['common']}.")
+# reg = []
+# for country in COUNTRIES:
+#     if country['region'] not in reg:
+#         reg.append(country['region'])
+
+# regions = {}
+# for r in reg:
+#     regions[r] = []
+# for country in COUNTRIES:
+#     regions[country['region']].append(country)
+
+REGIONS = {}
+for country in COUNTRIES:
+    if country['region'] in REGIONS:
+        REGIONS[country['region']].append(country)
+    else:
+        REGIONS[country['region']] = []
+        REGIONS[country['region']].append(country)
+
+
+def main():
+    play = True
+    while play:
+        country = random.choice(REGIONS[random.choice(list(REGIONS))])
+        while country['capital']:
+            hint = list(country['capital'][0])
+            user_guess = input(f"What is the capital of {country['name']['common']}? ")
+            if user_guess in country['capital']:
+                print("That's correct!")
+                country['capital'].remove(user_guess)
+            elif user_guess == "pass":
+                print(f"{country['capital']} was the right answer.")
+                break
+            elif user_guess == "q":
+                play = False
+                break
+            else:
+                print("That's wrong.")
+                print(f"Here's a hint: {('').join(hint[0:1])}")
+
+if __name__ == "__main__":
+    main()
